67139e107bef1db6863a63d8467f1c7f1597d422
Alexander Tsupko <alexander.tsupko@outlook.com>
2019-02-08 14:49:24 +0300

    Add printSeveralTimes(String, int) method

--- src/main/java/ru/inno/project/App.java
+++ src/main/java/ru/inno/project/App.java
@@ -1,7 +1,29 @@
 package ru.inno.project;
 
+/**
+ * Клиентский код.
+ */
 public class App {
+    private static final int N = 10; // количество повторений
+
+    /**
+     * Печатает заданное количество раз заданную строку.
+     *
+     * @param s строка, которую нужно напечатать
+     * @param n количество раз
+     */
+    private static void printSeveralTimes(String s, int n) {
+        while (n-- > 0) {
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * Точка входа в приложение.
+     *
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        printSeveralTimes("Hello, World!", N);
     }
 }

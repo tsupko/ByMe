aa2ebcdc55f38c89686906aed7e6c6e359b32417
AnastasiaVK <anastasia-vk1@ya.ru>
2019-02-13 23:05:11 +0300

    Added POJO classes

---
+++ src/main/java/ru/inno/project/entity/Image.java
@@ -0,0 +1,56 @@
+package ru.inno.project.entity;
+
+/**
+ * POJO класс для таблицы image
+ *
+ * @author Kuzina Anastasia
+ */
+public class Image {
+
+    private int id;
+    private String img;
+    private boolean is_main;
+    private int ad_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public boolean isIs_main() {
+        return is_main;
+    }
+
+    public void setIs_main(boolean is_main) {
+        this.is_main = is_main;
+    }
+
+    public int getAd_id() {
+        return ad_id;
+    }
+
+    public void setAd_id(int ad_id) {
+        this.ad_id = ad_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", img='" + img + '\'' +
+                ", is_main=" + is_main +
+                ", ad_id=" + ad_id +
+                '}';
+    }
+}
---
+++ src/main/java/ru/inno/project/entity/Role.java
@@ -0,0 +1,36 @@
+package ru.inno.project.entity;
+
+/**
+ * POJO класс для таблицы user
+ *
+ * @author Kuzina Anastasia
+ */
+public class Role {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}

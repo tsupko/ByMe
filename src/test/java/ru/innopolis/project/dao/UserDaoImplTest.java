990aa58855f907e0f4fc03b52bba15f11dbc08fd
AnastasiaVK <anastasia-vk1@ya.ru>
2019-02-14 14:46:23 +0300

    fix UserD

--- src/test/java/ru/innopolis/project/dao/UserDaoImplTest.java
+++ src/test/java/ru/innopolis/project/dao/UserDaoImplTest.java
@@ -5,36 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.innopolis.project.entity.User;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class UserDAOImplTest {
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserDAOImplTest.class);
+public class UserDaoImplTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserDaoImplTest.class);
     private static UserDao userDAO;
 
     private static void init() {
         LOGGER.info("init");
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        /*
-         *  параметры для подключения к БД
-         */
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String login = "postgres";
-        String pass = "";
-
-//        Connection connection = null;
-//        try {
-//            connection = DriverManager.getConnection(url, login, pass);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-
         userDAO = new UserDaoImpl();
     }
 

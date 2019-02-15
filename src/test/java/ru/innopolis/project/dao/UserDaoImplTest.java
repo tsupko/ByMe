3e7e37b09117010d59ae49a1fba15ce25767dcee
AnastasiaVK <anastasia-vk1@ya.ru>
2019-02-15 16:28:03 +0300

    Добавление теста для метода exists

--- src/test/java/ru/innopolis/project/dao/UserDaoImplTest.java
+++ src/test/java/ru/innopolis/project/dao/UserDaoImplTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.innopolis.project.entity.User;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserDaoImplTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserDaoImplTest.class);
@@ -21,10 +22,10 @@ public class UserDaoImplTest {
         LOGGER.info("create user test");
         User user = new User();
 
-        user.setLogin("test");
+        user.setLogin("test2");
         user.setPassword("123");
         user.setName("TTT");
-        user.setEmail("ttt@ya.ru");
+        user.setEmail("ttt2@ya.ru");
         user.setPhoneNumber("+70000000000");
         user.setRoleId(1);
         user.setCityId(1);
@@ -44,5 +45,34 @@ public class UserDaoImplTest {
         id = 1;
         user = userDAO.selectById(id);
         LOGGER.info("Selected user by id = " + id + " " + user.toString());
+    }
+
+    @Test
+    void existsTest() {
+        LOGGER.info("existsTest");
+        init();
+        String login = "admin";
+        String password = "admin";
+        userDAO.exists(login, password);
+        assertEquals(true, userDAO.exists(login, password));
+    }
+
+    @Test
+    void existsPositiveTest() {
+        LOGGER.info("existsTest");
+        init();
+        String login = "gfdsuygj";
+        String password = "sfd";
+        userDAO.exists(login, password);
+        assertEquals(false, userDAO.exists(login, password));
+    }
+
+    @Test
+    void existsNegativeTest() {
+        LOGGER.info("existsTest");
+        init();
+        String login = " ";
+        String password = " ";
+        userDAO.exists(login, password);
     }
 }

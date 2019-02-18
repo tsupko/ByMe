bc3060e84e3352d85ee1e0a054cfb9c4bec3c7f4
Dmitriy <dkom91@gmail.com>
2019-02-19 00:00:24 +0300

    добавил соль к паролю
    добавил зависимости на security

--- src/main/java/ru/innopolis/byme/controller/UserController.java
+++ src/main/java/ru/innopolis/byme/controller/UserController.java
@@ -2,6 +2,8 @@ package ru.innopolis.byme.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +14,15 @@ import ru.innopolis.byme.entity.User;
 
 @Controller
 public class UserController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
-    private UserDao dao;
 
-    public UserController(UserDao dao) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+    private PasswordEncoder encoder;
+    private final UserDao dao;
+
+    @Autowired
+    public UserController(UserDao dao, PasswordEncoder encoder) {
         this.dao = dao;
+        this.encoder = encoder;
     }
 
     @GetMapping("/registration")
@@ -28,6 +34,9 @@ public class UserController {
 
     @PostMapping("/registration")
     public String registration(@ModelAttribute("user") User user) {
+
+        user.setPassword(encoder.encode(user.getPassword()));
+
         LOGGER.info("Новый пользователь: {}", user);
         dao.create(user);
         return "redirect:/";

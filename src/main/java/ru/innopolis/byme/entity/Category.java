6d6e0b215af97a2bb115c76d90d481e38af7c640
AnastasiaVK <anastasia-vk1@ya.ru>
2019-02-21 11:23:52 +0300

    Added equals and hashCode methods in POJO classes

--- src/main/java/ru/innopolis/byme/entity/Category.java
+++ src/main/java/ru/innopolis/byme/entity/Category.java
@@ -1,5 +1,7 @@
 package ru.innopolis.byme.entity;
 
+import java.util.Objects;
+
 /**
  * POJO класс для таблицы category
  *
@@ -42,6 +44,21 @@ public class Category {
                 ", name='" + name + '\'' +
                 ", parentId=" + parentId +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id &&
+                parentId == category.parentId &&
+                name.equals(category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, parentId);
     }
 }
 
--- src/main/java/ru/innopolis/byme/entity/City.java
+++ src/main/java/ru/innopolis/byme/entity/City.java
@@ -1,5 +1,10 @@
 package ru.innopolis.byme.entity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
 /**
  * POJO класс для таблицы city
  *
@@ -32,5 +37,19 @@ public class City {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return id == city.id &&
+                name.equals(city.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }

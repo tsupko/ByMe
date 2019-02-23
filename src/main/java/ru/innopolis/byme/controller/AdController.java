af0d1c152a264ae5e0085fa0bc1e317e56f40d37
Alexey Balakin <kontrmail@gmail.com>
2019-02-23 15:51:55 +0300

    feature/1_adv_add_file - добавил загрузку фото для объявлений

--- src/main/java/ru/innopolis/byme/controller/AdController.java
+++ src/main/java/ru/innopolis/byme/controller/AdController.java
@@ -1,18 +1,27 @@
 package ru.innopolis.byme.controller;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import ru.innopolis.byme.dao.AdDao;
 import ru.innopolis.byme.dao.CategoryDao;
+import ru.innopolis.byme.dao.ImageDao;
 import ru.innopolis.byme.dao.UserDao;
 import ru.innopolis.byme.entity.Ad;
+import ru.innopolis.byme.entity.Image;
 import ru.innopolis.byme.entity.User;
+import ru.innopolis.byme.exception.ImageUploadException;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.Principal;
 
 @Controller
@@ -22,15 +31,19 @@ public class AdController {
     private final AdDao adDao;
     private final UserDao userDao;
     private final CategoryDao categoryDao;
+    private final ImageDao imageDao;
+    @Value("${upload.location}")
+    private String fileUpload;
 
-    public AdController(AdDao adDao, UserDao userDao, CategoryDao categoryDao) {
+    public AdController(AdDao adDao, UserDao userDao, CategoryDao categoryDao, ImageDao imageDao) {
         this.adDao = adDao;
         this.userDao = userDao;
         this.categoryDao = categoryDao;
+        this.imageDao = imageDao;
     }
 
     @RequestMapping(value = "/ad", method = RequestMethod.GET)
-    public String ad(Model model){
+    public String ad(Model model) {
         LOGGER.info("mapping get /ad");
         model.addAttribute("categories", categoryDao.getAll());
         model.addAttribute("ad", new Ad());
@@ -38,7 +51,8 @@ public class AdController {
     }
 
     @RequestMapping(value = "/ad", method = RequestMethod.POST)
-    public String addAd(@ModelAttribute("ad") Ad ad, Principal principal) {
+    public String addAd(@ModelAttribute("ad") Ad ad, MultipartFile image,
+                        BindingResult bindingResult, Principal principal) {
         String login = principal.getName();
         LOGGER.info("mapping post /ad, login: {}", login);
         User user = userDao.selectByLogin(login).get();
@@ -47,6 +61,43 @@ public class AdController {
         ad.setActual(true);
         LOGGER.info("Новое объявление: {}", ad);
         adDao.create(ad);
+        LOGGER.info("File name: " + image.getName());
+        LOGGER.info("File size: " + image.getSize());
+        LOGGER.info("File content type: " + image.getContentType());
+        LOGGER.info("image.isEmpty(): " + image.isEmpty());
+        try {
+            if (!image.isEmpty()) {
+                validateImage(image);
+                String imageName = ad.getId() + ".jpg";
+                saveImage(imageName, image);
+                Image img = new Image();
+                img.setImg(imageName);
+                img.setAdId(ad.getId());
+                img.setMain(true);
+                LOGGER.info("Новое фото объявление: {}", img);
+                imageDao.create(img);
+            }
+        } catch (ImageUploadException e) {
+            bindingResult.reject(e.getMessage());
+            return "ad";
+        }
         return "redirect:/";
+    }
+
+    private void validateImage(MultipartFile image) throws ImageUploadException {
+        if (!image.getContentType().equals("image/jpeg")) {
+            LOGGER.error("Only JPG images accepted");
+            throw new ImageUploadException("Only JPG images accepted");
+        }
+    }
+
+    private void saveImage(String filename, MultipartFile image) throws ImageUploadException {
+        try {
+            File file = new File(fileUpload + filename);
+            FileUtils.writeByteArrayToFile(file, image.getBytes());
+        } catch (IOException e) {
+            LOGGER.error("File save error ", e);
+            throw new ImageUploadException("Unable to save image", e);
+        }
     }
 }

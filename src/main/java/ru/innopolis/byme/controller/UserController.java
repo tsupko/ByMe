0b579ee7c46ac46576b4523ddfec63b6b51660de
Dmitriy <dkom91@gmail.com>
2019-02-25 10:20:04 +0300

    - добавлена страница авторизации - поправлена бага с регистрацией

--- src/main/java/ru/innopolis/byme/controller/UserController.java
+++ src/main/java/ru/innopolis/byme/controller/UserController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -62,5 +63,8 @@ public class UserController {
         return "home";
     }
 
-
+    @GetMapping("login")
+    public String login(){
+        return "login";
+    }
 }
